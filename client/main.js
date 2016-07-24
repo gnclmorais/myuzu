14071d22f2fd624a157015872226bf0ccf485804
Gonçalo Morais <gnclmorais@gmail.com>
2016-07-24 16:49:08 +0100

    Use Pug templating instead.

--- client/main.js
+++ client/main.js
@@ -1,8 +1,6 @@
 var $ = require('jquery');
 var Masonry = require('masonry-layout');
 
-console.log('main.js');
-
 $(window).on('load', function () {
   var msnry = new Masonry('.grid', {
     itemSelector: '.grid-item',
--- public/app.js
+++ public/app.js
@@ -2,8 +2,6 @@
 var $ = require('jquery');
 var Masonry = require('masonry-layout');
 
-console.log('main.js');
-
 $(window).on('load', function () {
   var msnry = new Masonry('.grid', {
     itemSelector: '.grid-item',
