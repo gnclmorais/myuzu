cc11286bb75356cd8570b645dd0b25c1d5ce3ffd
Gonçalo Morais <gnclmorais@gmail.com>
2016-07-24 15:50:29 +0100

    Finally, everything centred!

--- client/main.js
+++ client/main.js
@@ -1,12 +1,11 @@
-var $ = require('zeptojs');
+var $ = require('jquery');
 var Masonry = require('masonry-layout');
 
-document.addEventListener("DOMContentLoaded", function () {
-  var wrapper = document.querySelector('.grid');
-  var msnry = new Masonry(wrapper, {
+console.log('main.js');
+
+$(window).on('load', function () {
+  var msnry = new Masonry('.grid', {
     itemSelector: '.grid-item',
-    columnWidth: 200,
     fitWidth: true,
-    gutter: 10,
   });
 });
--- server.js
+++ server.js
@@ -1,11 +1,67 @@
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var express = require('express');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var path = require('path');
+var stylus = require('stylus');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-app.get('/', function(req, res){
-  res.render('index.hbs');
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(stylus.middleware({
+  src: __dirname + '/public',
+  compile: function compile(str, path) {
+    return stylus(str).set('filename', path);
+  },
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 app.listen(3000);
-console.log('Listening on port 3000');
+module.exports = app;
